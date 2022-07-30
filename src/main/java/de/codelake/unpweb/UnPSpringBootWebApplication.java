c050fc6433259666cba5a3de75440aff525282c5
Michael Kagel <michael.kagel@web.de>
2022-07-30 11:44:49 +0200

    Initial commit

---
+++ src/main/java/de/codelake/unpweb/UnPSpringBootWebApplication.java
@@ -0,0 +1,13 @@
+package de.codelake.unpweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnPSpringBootWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnPSpringBootWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/codelake/unpweb/UnPSpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package de.codelake.unpweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnPSpringBootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
