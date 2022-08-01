d4bd7ef416481d811884732dc74ae193ead639dc
Michael Kagel <michael.kagel@web.de>
2022-08-01 15:55:29 +0200

    Add test for get all persons

--- src/test/java/de/codelake/unpweb/PersonTest_IT.java
+++ src/test/java/de/codelake/unpweb/PersonTest_IT.java
@@ -1,6 +1,24 @@
 package de.codelake.unpweb;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+import de.codelake.unpweb.domain.dto.UnitDto;
+
 public class PersonTest_IT extends AbstractTest_IT {
 
+	@Test
+	@DisplayName("GET all persons")
+	public void readAllPersons() {
+		@SuppressWarnings("unchecked")
+		final var response = template.getForEntity("/persons", (Class<List<UnitDto>>) (Class<?>) List.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(response.getBody()).hasSize(26);
+	}
 
 }
